f1a7974f7c723d198a18593ed728d744c5f16cc0
roboticapples14 <natalievalett@gmail.com>
2023-03-11 13:27:09 +0100

    game interface

--- tests/test_Board.py
+++ tests/test_Board.py
@@ -12,6 +12,7 @@ from src.Board import Player
 class TestBoard(unittest.TestCase):
 
     def test_move(self):
+        print("Running test_move...")
         game = Checkers()
         game._create_place_piece("b", (2, 1))
         game._create_place_piece("w", (3, 2))
@@ -22,8 +23,10 @@ class TestBoard(unittest.TestCase):
         test2 = game.board[4][3]
         self.assertEqual(test1, Pieces.EMPTY)
         self.assertEqual(test2, Pieces.BLACK)
+        print("Success")
 
     def test_move_advanced(self):
+        print("Running test_move_advanced...")
         game = Checkers()
         game._create_place_piece("b", (2, 1))
         game._create_place_piece("w", (3, 2))
@@ -37,8 +40,10 @@ class TestBoard(unittest.TestCase):
         self.assertEqual(test1, Pieces.EMPTY)
         self.assertEqual(test2, Pieces.EMPTY)
         self.assertEqual(test3, Pieces.BLACK)
+        print("Success")
 
     def test_move_opposite_direction(self):
+        print("Running test_move_opposite_direction...")
         game = Checkers()
         game._create_place_piece("w", (2, 1))
         game._create_place_piece("w", (4, 3))
@@ -52,8 +57,10 @@ class TestBoard(unittest.TestCase):
         self.assertEqual(test1, Pieces.EMPTY)
         self.assertEqual(test2, Pieces.EMPTY)
         self.assertEqual(test3, Pieces.BLACK)
+        print("Success")
 
     def test_move_3rd_direction(self):
+        print("Running test_move_3rd_direction...")
         game = Checkers()
         game._create_place_piece("w", (1, 2))
         game._create_place_piece("w", (3, 4))
@@ -67,8 +74,10 @@ class TestBoard(unittest.TestCase):
         self.assertEqual(test1, Pieces.EMPTY)
         self.assertEqual(test2, Pieces.EMPTY)
         self.assertEqual(test3, Pieces.BLACK)
+        print("Success")
 
     def test_move_king_piece(self):
+        print("Running test_move_king_piece...")
         game = Checkers()
         game._create_place_piece("w", (6, 6))
         game.turn = Player.WHITE
@@ -77,35 +86,44 @@ class TestBoard(unittest.TestCase):
         game.printBoard()
         test1 = game.board[7][7]
         self.assertEqual(test1, Pieces.WHITE_KING)
+        print("Success")
     
     def test_valid_move(self):
+        print("Running test_valid_move...")
         game = Checkers()
         game._create_place_piece("w", (2, 1))
         game._create_place_piece("b", (3, 2))
         valid = game.validMove((3, 2), (1, 0))
         self.assertTrue(valid)
+        print("Success")
 
     # tries to move other player's piece
     def test_invalid_move(self):
+        print("Running test_invalid_move...")
         game = Checkers()
         game._setupBoard("pieces")
         with self.assertRaises(ValueError):
             game.validMove((0, 0), (4, 3))
+        print("Success")
 
     # tries to move other player's piece
     def test_valid_jump(self):
+        print("Running test_valid_jump...")
         game = Checkers()
         game._setupBoard("pieces")
         game._create_place_piece("w", (4, 2))
         valid = game.validMove((5, 1), (3, 3))
         self.assertTrue(valid)
+        print("Success")
 
     def test_isFinished(self):
+        print("Running test_isFinished...")
         game = Checkers()
         game._setupBoard("pieces")
         game.isFinished()
         self.assertFalse(game.game_ended)
         self.assertEqual(game.win, Player.NONE)
+        print("Success")
 
 test = TestBoard()
 test.test_move()
